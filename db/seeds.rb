8bae3cfca966f12183e6a743a75ae74291755ef3
LeonardodeMello <leorudas@gmail.com>
2024-08-27 11:32:05 -0300

    validation_cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.destroy_all
+
+User.create!(email: 'user@user.com', password: '123456', first_name: 'Julinho', last_name: 'Bala')

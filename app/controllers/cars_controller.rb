f11994b7f8c85ca9761ec85f3f40eeef555a4b63
Daniel Muller Moutinho <dan.muller141@gmail.com>
2024-08-27 17:35:08 -0300

    cloudnary and car forms

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,11 +7,26 @@ class CarsController < ApplicationController
   end
 
   def new
+    @car = Car.new
   end
 
   def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save
+      redirect_to car_path(@car), :notice => "Seu carro foi salvo com sucesso!"
+    else
+      flash.now[:notice] = "Não foi possivel salvar o carro. Por favor reveja as informações..."
+      render :new
+    end
   end
 
   def delete
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :year, :km, :photo)
   end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,7 @@
 class Car < ApplicationRecord
   has_many :deals
   belongs_to :user
+  has_one_attached :photo
 
   validates :brand, :model, :km, presence: true
 end

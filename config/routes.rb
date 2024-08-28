8aa346804c28b33c131538d29c200e1e9a11295a
LeonardodeMello <leorudas@gmail.com>
2024-08-28 15:55:28 -0300

    profile_page

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :cars, except: %i[edit update]
+  resource :profiles, only: :show
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
 end
